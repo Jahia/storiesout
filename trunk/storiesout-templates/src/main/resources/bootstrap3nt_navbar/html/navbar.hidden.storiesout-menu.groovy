b605169c1c9766df4ad43a3b5ba5573606e6c4ed
pvollenweider <pvollenweider@e5868cd5-3091-49c7-b3dc-3821f0fadf29>
2015-11-13 15:23:10 +0000

    Add hide this page

--- trunk/storiesout-templates/src/main/resources/bootstrap3nt_navbar/html/navbar.hidden.storiesout-menu.groovy
+++ trunk/storiesout-templates/src/main/resources/bootstrap3nt_navbar/html/navbar.hidden.storiesout-menu.groovy
@@ -12,70 +12,71 @@ printMenu = { node, navMenuLevel ->
 
         children.eachWithIndex() { menuItem, index ->
             if (menuItem != null) {
-                def itemPath = menuItem.path;
-                def correctType = true;
-                if (menuItem.isNodeType("jmix:navMenu")) {
-                    correctType = false;
-                }
-                if (menuItem.properties['j:displayInMenu']) {
-                    correctType = false;
-                    menuItem.properties['j:displayInMenu'].each() {
-                        correctType |= (it.node.identifier == currentNode.identifier)
+                if (! menuItem.isNodeType("somix:hidePage")) {
+                    def itemPath = menuItem.path;
+                    def correctType = true;
+                    if (menuItem.isNodeType("jmix:navMenu")) {
+                        correctType = false;
                     }
-                }
-                if (correctType) {
-                    hasChildren = navMenuLevel < maxDepth && JCRTagUtils.hasChildrenOfType(menuItem, "jnt:page,jnt:nodeLink,jnt:externalLink")
-                    Resource resource = new Resource(menuItem, "html", "menuElement-dropdown", currentResource.getContextConfiguration());
-                    def render = RenderService.getInstance().render(resource, renderContext)
-                    if (render != "") {
-                        def inpath = renderContext.mainResource.node.path == itemPath || renderContext.mainResource.node.path.startsWith(itemPath + "/");
-                        def active = menuItem.isNodeType("jmix:nodeReference") ?
-                                renderContext.mainResource.node.path == menuItem.properties['j:node'].node.path :
-                                renderContext.mainResource.node.path == itemPath;
-                        listItemCssClass = "class=\"" + (inpath || active ? "active " : "") + (hasChildren && navMenuLevel > 1 ? "dropdown-menu" : "dropdown") + "\"";
-                        description = menuItem.properties['jcr:description'];
-                        linkTitle = description ? " title=\"${description.string}\"" : ""
-                        link = menuItem.url;
-                        if (navMenuLevel == 1   ) {
-                            if (!ulIsOpen) {
-                                ulClass = "nav navbar-nav navbar-right";
-                                if (currentNode.isNodeType("bootstrap3mix:navbarAdvanced")) {
-                                    ulClass = currentNode.properties['ulClass'].string;
+                    if (menuItem.properties['j:displayInMenu']) {
+                        correctType = false;
+                        menuItem.properties['j:displayInMenu'].each() {
+                            correctType |= (it.node.identifier == currentNode.identifier)
+                        }
+                    }
+                    if (correctType) {
+                        hasChildren = navMenuLevel < maxDepth && JCRTagUtils.hasChildrenOfType(menuItem, "jnt:page,jnt:nodeLink,jnt:externalLink")
+                        Resource resource = new Resource(menuItem, "html", "menuElement-dropdown", currentResource.getContextConfiguration());
+                        def render = RenderService.getInstance().render(resource, renderContext)
+                        if (render != "") {
+                            def inpath = renderContext.mainResource.node.path == itemPath || renderContext.mainResource.node.path.startsWith(itemPath + "/");
+                            def active = menuItem.isNodeType("jmix:nodeReference") ?
+                                    renderContext.mainResource.node.path == menuItem.properties['j:node'].node.path :
+                                    renderContext.mainResource.node.path == itemPath;
+                            listItemCssClass = "class=\"" + (inpath || active ? "active " : "") + (hasChildren && navMenuLevel > 1 ? "dropdown-menu" : "dropdown") + "\"";
+                            description = menuItem.properties['jcr:description'];
+                            linkTitle = description ? " title=\"${description.string}\"" : ""
+                            link = menuItem.url;
+                            if (navMenuLevel == 1) {
+                                if (!ulIsOpen) {
+                                    ulClass = "nav navbar-nav navbar-right";
+                                    if (currentNode.isNodeType("bootstrap3mix:navbarAdvanced")) {
+                                        ulClass = currentNode.properties['ulClass'].string;
+                                    }
+                                    println "<ul class=\"${ulClass}\">\n"
+                                    ulIsOpen = true;
                                 }
-                                println "<ul class=\"${ulClass}\">\n"
-                                ulIsOpen = true;
-                            }
-                            if (hasChildren) {
-                                print "<li ${listItemCssClass}>";
-                                print "    <a href=\"${link}\" data-toggle=\"dropdown\" role=\"button\" aria-expanded=\"false\" ${linkTitle}>";
-                                print menuItem.displayableName;
-                                if (hasChildren && navMenuLevel < maxDepth) {
-                                    print "<span class=\"caret\"></span>";
+                                if (hasChildren) {
+                                    print "<li ${listItemCssClass}>";
+                                    print "    <a href=\"${link}\" data-toggle=\"dropdown\" role=\"button\" aria-expanded=\"false\" ${linkTitle}>";
+                                    print menuItem.displayableName;
+                                    if (hasChildren && navMenuLevel < maxDepth) {
+                                        print "<span class=\"caret\"></span>";
+                                    }
+                                    print "</a>";
+                                    if (hasChildren && navMenuLevel < maxDepth) {
+                                        printMenu(menuItem, navMenuLevel + 1);
+                                    }
+                                    println "</li>";
+                                } else {
+                                    println "<li ${listItemCssClass}><a href=\"${link}\"${linkTitle}> " + menuItem.displayableName + "</a></li>\n";
                                 }
-                                print "</a>";
+                            } else {
+                                if (!ulIsOpen) {
+                                    println "<ul class=\"dropdown-menu\">"
+                                    ulIsOpen = true;
+                                }
+                                println "<li ${listItemCssClass}>";
+                                print "<a href=\"${link} ${linkTitle}\">" + menuItem.displayableName + "</a>";
                                 if (hasChildren && navMenuLevel < maxDepth) {
                                     printMenu(menuItem, navMenuLevel + 1);
                                 }
-                                println "</li>";
-                            } else {
-                                println "<li ${listItemCssClass}><a href=\"${link}\"${linkTitle}> " + menuItem.displayableName + "</a></li>\n";
-                            }
-                        } else {
-                            if (!ulIsOpen) {
-                                println "<ul class=\"dropdown-menu\">"
-                                ulIsOpen = true;
-                            }
-                            println "<li ${listItemCssClass}>";
-                            print "<a href=\"${link} ${linkTitle}\">" + menuItem.displayableName + "</a>";
-                            if (hasChildren && navMenuLevel < maxDepth) {
-                                printMenu(menuItem, navMenuLevel + 1);
-                            }
 
-                            println "</li>";
+                                println "</li>";
+                            }
                         }
                     }
                 }
-
             }
         }
         if (ulIsOpen) {
